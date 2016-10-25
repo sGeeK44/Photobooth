2e93a5f9d5df0e62a37f356eb99b9d5b7fbec17e
PERIN François <f.perin@zero-gachis.com>
2016-10-25 08:47:35 +0200

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows;
+using PhotoBooth.Properties;
+
+namespace PhotoBooth
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        private const int Hide = 0;
+        private const int Minimized = 6;
+        private const int Maximized = 3;
+        private const int Show = 5;
+
+        private readonly IntPtr _mainWindowsHandle;
+        private readonly Process _photoBooth;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            VideoElement.Source = new Uri(Settings.Default.VideoSource);
+            VideoElement.MediaEnded += (sender, args) => {
+                VideoElement.Stop();
+                ShowWindow(_mainWindowsHandle, Maximized);
+                Thread.Sleep(10000);
+                ShowWindow(_mainWindowsHandle, Hide);
+            };
+            _photoBooth = Process.Start(Settings.Default.PhotoBoothExecPath);
+            Thread.Sleep(5000);
+            _mainWindowsHandle = _photoBooth.MainWindowHandle;
+            ShowWindow(_mainWindowsHandle, Hide);
+            Closed += (sender, args) => _photoBooth.Kill();
+        }
+
+        private void Play_OnClick(object sender, RoutedEventArgs e)
+        {
+            VideoElement.Play();
+        }
+    }
+}
---
+++ MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows;
+using PhotoBooth.Properties;
+
+namespace PhotoBooth
+{
+    public class MainWindowViewModel
+    {
+
+    }
+}

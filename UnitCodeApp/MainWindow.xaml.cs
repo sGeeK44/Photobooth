36fa377025bd29fef1882e7ea00b0c24711ed872
PERIN François <f.perin@zero-gachis.com>
2017-04-12 23:59:31 +0200

    Init UnitCode App

---
+++ UnitCodeApp/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.Printing;
+using System.Windows;
+using CabineParty.Core;
+using CabineParty.Core.Windows;
+using CabineParty.UnitCodeApp.Properties;
+
+namespace CabineParty.UnitCodeApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private WindowsManager _mainWindows;
+        private Process _photoBooth;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartPhotobooth();
+            Closed += ExitPhotobooth;
+            SetupPrinter();
+        }
+
+        private void ExitPhotobooth(object sender, EventArgs args)
+        {
+            try
+            {
+                if (!_photoBooth.HasExited)
+                    _photoBooth.Kill();
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+
+        private void SetupPrinter()
+        {
+            var printQueueMonitor = new PrintQueueMonitor(new LocalPrintServer().DefaultPrintQueue.FullName);
+            printQueueMonitor.Start();
+            printQueueMonitor.OnJobAdded += () =>
+            {
+                // TODO add log checker.
+                _mainWindows.Show();
+            };
+        }
+        
+        private void StartPhotobooth()
+        {
+            ProcessHelper.KillAll(Settings.Default.PhotoBoothProcessName);
+            _photoBooth = ProcessHelper.Start(Settings.Default.PhotoBoothExecPath);
+            _mainWindows = new WindowsManager(_photoBooth);
+            _mainWindows.Hide();
+        }
+    }
+}
---
+++ UnitCodeApp/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Media;
+using CabineParty.Core.Mvvm;
+using CabineParty.UnitCodeApp.Properties;
+
+namespace CabineParty.UnitCodeApp
+{
+    public class MainWindowViewModel : ViewModel
+    {
+        private readonly Color _defaultBackGroundColor = Colors.Beige;
+
+        public Brush BackgroundColor
+        {
+            get
+            {
+                try
+                {
+                    var color = ColorConverter.ConvertFromString(Settings.Default.BackgroundColor)
+                             ?? _defaultBackGroundColor;
+
+                    return new SolidColorBrush((Color)color);
+
+                }
+                catch (Exception)
+                {
+                    return new SolidColorBrush(_defaultBackGroundColor);
+                }
+            }
+        }
+    }
+}

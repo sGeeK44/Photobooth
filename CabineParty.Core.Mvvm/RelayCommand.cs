ded0ef5db59d00208eded00df4ce07be2d57e231
PERIN François <f.perin@zero-gachis.com>
2017-04-12 23:00:49 +0200

    Add Mvvm dll project

--- CabineParty.Core.Mvvm/RelayCommand.cs
+++ CabineParty.Core.Mvvm/RelayCommand.cs
@@ -2,7 +2,7 @@
 using System.Diagnostics;
 using System.Windows.Input;
 
-namespace CabineParty.ShowRoomApp
+namespace CabineParty.Core.Mvvm
 {
     /// <summary>
     /// A command whose sole purpose is to relay its functionality to other objects by invoking delegates.
